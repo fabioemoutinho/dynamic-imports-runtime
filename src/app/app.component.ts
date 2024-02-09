3aea05b1a3380229fe0cc2e3fef8aa708c1a6910
fabioemoutinho <fabioemoutinho@gmail.com>
2024-02-09 11:47:57 -0300

    minimal reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,9 @@ export class AppComponent {
   title = 'dynamic-imports-runtime';
 
   constructor() {
-    import('https://code.jquery.com/jquery-3.7.1.min.js').then((jquery) => {
-      console.log('jquery loaded');
-      console.log(jquery.fn.jquery);
-    });
+    // @ts-ignore
+    import('https://code.jquery.com/jquery-3.7.1.min.js').then((jquery) =>
+      console.log(jquery)
+    );
   }
 }
